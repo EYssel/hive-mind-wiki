a34ed0e44708ad7c5fa62f6c508fbb3bd14c97b0
Estian Yssel <estianyssel@gmail.com>
2023-04-23 17:41:59 +0200

    Add api deploy

--- apps/api/cdk/api.stack.ts
+++ apps/api/cdk/api.stack.ts
@@ -1,1 +1,18 @@
-export class ApiStack {}
+import * as cdk from 'aws-cdk-lib';
+import { Architecture, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+
+export class ApiStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    new NodejsFunction(this, 'api', {
+      entry: 'apps/api/src/main.ts',
+      handler: 'handler',
+      memorySize: 128,
+      architecture: Architecture.ARM_64,
+      runtime: Runtime.NODEJS_18_X,
+    });
+  }
+}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { Context, Handler } from 'aws-lambda';
+import { createServer, proxy } from 'aws-serverless-express';
+import { eventContext } from 'aws-serverless-express/middleware';
+import { Server } from 'http';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+const express = require('express');
+
+const binaryMimeTypes: string[] = [];
+
+let cachedServer: Server;
+
+async function bootstrapServer(): Promise<Server> {
+  if (!cachedServer) {
+    const expressApp = express();
+    const nestApp = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(expressApp),
+    );
+    nestApp.use(eventContext());
+    await nestApp.init();
+    cachedServer = createServer(expressApp, undefined, binaryMimeTypes);
+  }
+  return cachedServer;
 }
-bootstrap();
+
+export const handler: Handler = async (event: any, context: Context) => {
+  cachedServer = await bootstrapServer();
+  return proxy(cachedServer, event, context, 'PROMISE').promise;
+};
