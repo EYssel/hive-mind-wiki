c5bd53521d4064ce9954e1fab55787f8dbae21a8
Estian Yssel <estianyssel@gmail.com>
2023-04-23 18:11:22 +0200

    fix aws cdk

--- apps/api/cdk/api.stack.ts
+++ apps/api/cdk/api.stack.ts
@@ -2,6 +2,8 @@ import * as cdk from 'aws-cdk-lib';
 import { Architecture, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 
+const app = new cdk.App();
+
 export class ApiStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -17,7 +19,9 @@ export class ApiStack extends cdk.Stack {
   }
 }
 
-const app = new cdk.App();
 new ApiStack(app, 'InfrastructureStack', {
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: {
+    account: process.env.AWS_ACCOUNT_NUMBER,
+    region: process.env.AWS_REGION,
+  },
 });
